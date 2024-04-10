4e2b176834609ead02200d3ba51db89843aebb3f
duffy walsh <ryan.walsh@mongodb.com>
2024-04-10 13:53:57 -0600

    Addued merge back to a third collection

--- lookup/simple_lookup.js
+++ lookup/simple_lookup.js
@@ -1,11 +1,30 @@
-//data to be lookedup
+// remmeber to enable change stream sources
+// https://github.com/josephxsxn/atlas_stream_processors
+//
+// drop some dummy documents in the two collections we will use
+// so we can run a for loop over all collections in our demo db
+// and enable them
+db.people.insertOne({name : "Ryan", company : "MongoDB"})
+db.data.insertOne({name : "Duffy" })
+var cols = db.getCollectionNames()
+for (const el of cols){
+    db.runCommand( {
+        collMod: el,
+        changeStreamPreAndPostImages: { enabled: true }
+    } )
+}
+
+// use collection 'asp_examples'
+use asp_examples
+
+// data to be lookedup
 db.people.insertOne({name : "Joe", company : "MongoDB"})
 
 
 s = {
   $source:  {
-      connectionName: 'jsncluster0',
-      db: 'test',
+      connectionName: 'aspDemoConnection',
+      db: 'asp_examples',
       coll: 'data',
       config : {
             fullDocument: 'required',
@@ -17,8 +36,8 @@ s = {
 l = {
   $lookup: {
       from: {
-          connectionName: 'jsncluster0',
-          db: 'test',
+          connectionName: 'aspDemoConnection',
+          db: 'asp_examples',
           coll: 'people'
       },
       localField: "name",
@@ -27,7 +46,33 @@ l = {
   }
 }
 
-sp.process([s,l])
+sp.createStreamProcessor('asp_simple_lookup', [s,l, {
+    $merge: { 
+        into: {
+            connectionName: 'aspDemoConnection', 
+            db: 'asp_examples', 
+            coll: 'matched'
+        } 
+    }
+}])
+
+sp.asp_simple_lookup.start()
+
 
 //document for stream processor
 db.data.insertOne({name : "Joe"})
+
+
+--Merge to a Atlas Cluster Collection
+merge = {
+    $merge: {
+        into: {
+            connectionName: 'aspDemoConnection',
+            db: 'asp_examples',
+            coll: 'matched'
+        },
+        on: ['name'],
+        whenMatched: 'merge',
+        whenNotMatched: 'insert'
+    }
+}
